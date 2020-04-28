ca65e15f6dc35b10df46a068f2a959750e79865b
Rich Neswold <neswold@fnal.gov>
2020-04-28 12:54:51 -0500

    Prep for v0.2.0.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="acnet",
-    version="0.1.0",
+    version="0.2.0",
     author="Rich Neswold",
     author_email="neswold@fnal.gov",
     description="ACNET Client library",
