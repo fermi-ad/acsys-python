0fd9c354e199050f15a737153bff7690dc5fb807
Rich Neswold <neswold@fnal.gov>
2020-05-01 16:34:14 -0500

    Ths code does a Service Discovery and Open List.

--- acsys/dpm/__init__.py
+++ acsys/dpm/__init__.py
@@ -6,18 +6,41 @@ _log = logging.getLogger('asyncio')
 
 class DPM():
     def __init__(self, con, node):
-        self.desired_node = node
-        self.active_node = None
+        self.desired_node = node or 'MCAST'
+        self.dpm_task = None
         self.list_id = None
         self.con = con
+        self.gen = None
+
+    async def _find_dpm(self):
+        task = 'DPMD@' + self.desired_node
+        msg = ServiceDiscovery_request()
+        try:
+            replier, _, _ = await self.con.request_reply(task, msg,
+                                                         proto=dpm_protocol)
+            self.dpm_task = 'DPMD@' + (await self.con.get_name(replier))
+            _log.info('using DPM task: %s', self.dpm_task)
+        except:
+            self.dpm_task = None
+            raise
 
     async def _connect(self):
-        print('_connect()')
-        pass
+        await self._find_dpm()
+
+        # Send an OPEN LIST request to the DPM.
+
+        gen = self.con.request_stream(self.dpm_task, OpenList_request(),
+                                      proto=dpm_protocol)
+        _, _, msg = await gen.asend(None)
+        _log.info('DPM returned list id %d', msg.list_id)
+
+        # Update object state.
+
+        self.gen = gen
+        self.list_id = msg.list_id
 
     async def _shutdown(self):
-        print('_shutdown()')
-        pass
+        await self.gen.aclose()
 
 class DPMContext():
     def __init__(self, con, *, dpm_node=None):
