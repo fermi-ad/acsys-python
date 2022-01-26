b3d2667186c56928694703a7e5555c03122d792d
jbadillo <julianbadillo@gmail.com>
2022-01-26 14:40:43 -0600

    :fix: tests

--- tests/test_status.py
+++ tests/test_status.py
@@ -10,3 +10,8 @@ except AcnetReplyTaskDisconnected as ex:
     print(f"Captured exception: {ex!r}")
 except Status as st2:
     print(f"Captured any status: {st2!r}")
+
+try:
+    raise Status.create(1+256*-6)
+except AcnetRequestTimeOutQueuedAtDestination as ex:
+    print(f"Captured exception: {ex!r}")
